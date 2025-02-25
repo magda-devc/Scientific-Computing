ed2bb54ce3b5c806ae4c92e0a2f4c0a83dadf2f2
magda-devc <mgchebet@gmail.com>
2025-02-25 22:59:45 +0300

    First commit

---
+++ python_basics/datatypes.py
@@ -0,0 +1,23 @@
+number = int(5)
+floatVar = float(3.42)
+complexVar = complex(7, 2)
+listVar = list(['Oranges', 'Apples', 'Bananas'])
+tupleVAr = tuple((22,55,65,3))
+dictVar = dict({"Adidas": 9, "Nike": 10, "Puma": 8})
+setVar = set({'Apple', 'Banana', 'Orange'})
+boolVar = bool(True)
+
+print(type(number))
+print(type(floatVar))
+print(type(complexVar))
+print(type(listVar))
+print(type(tupleVAr))
+print(type(dictVar))
+print(type(setVar))
+print(type(boolVar))
+
+numberFloat = float(number)
+floatToInt = int(floatVar)
+
+print(type(numberFloat))
+print(type(floatToInt))
